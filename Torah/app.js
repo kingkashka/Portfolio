d714ff9370886fb64af1bf55f08ec5a104ebebad
nicholas keyes <bubbzthegreat@gmail.com>
2023-11-25 23:56:26 -0600

    first rendition of torah

---
+++ Torah/app.js
@@ -0,0 +1,15 @@
+const book1 = document.getElementById("book1").addEventListener("click", function(){
+
+})
+const book2 = document.getElementById("book1").addEventListener("click", function(){
+    
+})
+const book3 = document.getElementById("book1").addEventListener("click", function(){
+    
+})
+const book4 = document.getElementById("book1").addEventListener("click", function(){
+    
+})
+const book5 = document.getElementById("book1").addEventListener("click", function(){
+    
+})
