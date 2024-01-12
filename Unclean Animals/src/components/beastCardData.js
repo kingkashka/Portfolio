3fe1ed7dc7ea77473e9f2d1354a5a4ede66920c8
nicholas keyes <bubbzthegreat@gmail.com>
2024-01-11 23:40:03 -0600

    added data pages for a cleaner app component

---
+++ Unclean Animals/src/components/beastCardData.js
@@ -0,0 +1,26 @@
+const beastData =[
+    {
+     image: '/src/images/beast images/camel.jpg',
+     title: "הגמל - The camel",
+     description: "GAMAL ( גמל ). This terrestrial animal is definitely the camel but since the camel is not a ruminant and does not regurgitate its food the European Jewish and Christian bible translations which say mylath means “regurgitate” and that garah means “cud” are in error. The camel is a herbivore with two large, evenly sized toes. It has hooves that are the small nails at the tip of each toe, but it does not have a divided hoof or split hoof, which is a hoof split into two toes. Thus, it has the right diet but the wrong foot anatomy to be considered a clean food source."
+    },{
+     image: '/src/images/beast images/zebra.jpg',
+     title: "השפן - The Zebra",
+     description: "SHAPAN ( שפן ). This terrestrial animal is a herbivore with an undivided hoof and so must be a member of the Equus genus. The European Jewish and Christian bible versions which say this animalis the “coney” or “rock-hyrax” are in error since neither animal has a hoof of any kind. These animals also do not regurgitate their food, which is a major red flag that the translation “cheweth the cud” was wrong from the very start even if this animal was a coney or rock-hyrax. The Ghabaray word shapan gives us the correct answer. It means “to hide, to cover,” and since we are dealing with a member of the Equus genus the only equine that fits this description is the zebra. The zebra's conspicuous stripes act as a cover (a defense mechanism) against insects that bite and suck bloodsuch as menacing horseflies and tsetse flies. These insects are less likely to land on black-and-white striped surfaces than uniformly colored ones."
+    },{ 
+     image: '/src/images/beast images/horse.jpg',
+     title: "הארנבת - The Wild Horse",
+     description: "ARAN-BATH ( ארנבת ). This terrestrial animal is a herbivore with an undivided hoof. Again, we are dealing with a member of the Equus genus. Therefore, the European Jewish and Christian bible translations which say the aran-bath is the “hare” are in error since the hare does not have a hoof of any kind. The hare also does not regurgitate its food, which means even if hare was the correct animal mentioned in this verse the translation of mylath garah as “cheweth the cud” would still be inaccurate. Hence the theory that arnab, the common Arabic word for “hare,” is supposedly from the same root as the Ghabaray word aran-bath may be safely dismissed. The untamed wild horse asopposed to the domesticated horse is in view here. The word aran-bath combines two words. The word aran means “stridulous”. It is the scream or shrill creaking sound wild animals make, and also the sound heard from lofty trees with thick leaves when hit by strong winds. The word bath means “daughter”. Thus, aran-bath means “the scream of a daughter”. It is quite common to hear untamed horses scream in the wild, but it is very rare to hear a domesticated horse scream."
+    },{
+     image: '/src/images/beast images/pigs.jpg', 
+     title: "החזיר - The Pig",
+     description: `KHAZYR ( חזיר ). “And the KHAZAYR, for one dividing the hoof it is, and cleaving the cleft hoof, and just that foliage not willit chew, unclean it is to you all” (Thorah 3.11:7). The pig, swine. Although the pigs have split hooves and functional dewclaws,
+      and although they will forage for leaves, grass, roots, fruits and flowers like herbivores they areomnivorous scavengers and will eat plants, animals, and carrion. Thus, the pig has the right foot anatomy to be considered a clean food source but because of its diet and how it does not subsist onfoliage alone it is unclean among the hoofed terrestrial animals.`
+    },{
+     image: '/src/images/beast images/Marine .jpg',
+     title: "Everything in the waters",
+     description: "This will you all eat from all which are in the waters: everything which to it has protruding fins and scales in the waters, in the seas, and in the rivers, them will you all eat. And everything which does not have to it protruding fins and scales in the seas, and in the rivers, from all that swarm in the waters,and from every living creature which is in the waters, an abomination they are to you all. Even an abomination will they be to you all, from their flesh not will you all eat, and their dead corpses will you all be repulsed. All which does not have to it protruding spines and scales in the waters, an abomination it is to you all (Thorah 3.11:9-12)."
+    }
+    
+]
+export default beastData
---
+++ Unclean Animals/src/components/flyerCardData.js
@@ -0,0 +1,67 @@
+const flyerCardData = [
+    
+    { 
+     image: '/src/images/small images/eagle.jpg', 
+     title: 'NESHER ( נשר ). The eagle.'},
+    {
+     image: '/src/images/small images/vulture.jpg', 
+     title: 'PARAS ( פרס ). The vulture.'},
+    {
+     image: '/src/images/small images/buzzard.jpg',
+     title: 'AHZNYAH ( עזניה ). The buzzard.'},
+    {
+     image: '/src/images/small images/falcon.jpg', 
+     title: "DA'AH ( דאה ). The falcon."},
+    { 
+     image: '/src/images/small images/kite.jpg', 
+     title: 'AYAH ( איה ). The kite.'},
+    {
+     image: '/src/images/small images/raven.jpeg', 
+     title: 'OREV ( ערב ). The raven, crow.'},
+    { 
+     image: '/src/images/small images/ostritch.jpg', 
+     title: "BATH HA-YA'EHNAH ( בת היענה ). The ostritch."},
+    { 
+     image: '/src/images/small images/hawk.jpg', 
+     title: 'THAKHMAS ( תחמס ). The hawk.'},
+    {
+     image: '/src/images/small images/seagull.jpg', 
+     title: 'SHAKHAF ( שחף ). The seagull.'},
+    { 
+     image: '/src/images/small images/osprey5.jpg', 
+     title: 'NATS ( נץ ). The osprey.'},
+    { 
+     image:'/src/images/small images/nightOwl.jpg', 
+     title: 'CHOS ( כוס ). The little owl.'},
+    {
+     image:'/src/images/small images/cormorant.jpg', 
+     title: 'SHALACH ( שלך ). the cormorant.'},
+    { 
+     image: '/src/images/small images/barnOwl.jpg', 
+     title: 'YANASHOF ( ינשוף ). The barn-owl.'},
+    {
+     image: '/src/images/small images/lemur.jpg', 
+     title: 'THANASH-MATH ( תנשמת ). The flying colugos.'},
+    { 
+     image: '/src/images/small images/pelican.jpg', 
+     title: `KA'ATH ( קאת ). The pelican.`},
+    {
+     image: '/src/images/small images/crane.jpg', 
+     title: 'RAKHAM ( רחם ). The crane.'},
+    { 
+     image: '/src/images/small images/stork.jpg', 
+     title: `KHASYDAH ( חסידה ). The stork`},
+     
+    {image:'/src/images/small images/heron.jpg', 
+     title: 'ANAPAH ( אנפה ). The heron.'},
+    
+    {
+     image: '/src/images/small images/hoopoe.jpg', 
+     title: 'DOCH-YAFATH ( דוכיפת ). The hoopoe.'},
+      
+    {image: '/src/images/small images/bat.jpg',
+     title: 'AGHTLAF ( עטלף ). The bat.'}
+     
+]
+export default flyerCardData
+
